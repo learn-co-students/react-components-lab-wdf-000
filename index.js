2d2fece29356fd783fdc71542999613f85d8fcd4
sami4thewin <samiomarmohammed@gmail.com>
2017-03-01 08:25:44 -0500

    working

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ];
 
+const OlderCoaster = React.createClass(
+  render() {
+    return React.createElement('div', { className: 'oldercoaster'},
+      [
+        React.createElement('p', {}, 'Two grannies having the time of their life!'),
+        React.createElement('p', {}, 'Passengers:'),
+        React.createElement('ul', {},
+          [
+            React.createElement('li', {}, 'Agnes'),
+            React.createElement('li', {}, 'Muriel')
+          ]
+        );
+      ]
+    );
+  }
+);
+
+
 ReactDOM.render(
   React.createElement('div', {}, [
     React.createElement(OlderCoaster),
